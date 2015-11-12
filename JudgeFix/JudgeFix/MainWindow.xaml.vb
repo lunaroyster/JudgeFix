bb9ac5f4d96db9632e8eb05ae4acfc59a29638a6
lunaroyster <oneinaquadrillion@live.com>
2015-11-12 22:51:11 +0530

    Added minimize, maximize and close buttons

--- JudgeFix/JudgeFix/MainWindow.xaml.vb
+++ JudgeFix/JudgeFix/MainWindow.xaml.vb
@@ -1,15 +1,30 @@
 ﻿
 Class MainWindow
+    Private Sub b_minimize_Click(sender As Object, e As RoutedEventArgs) Handles b_minimize.Click
+        Me.WindowState = WindowState.Minimized
+    End Sub
 
-#Region "SQL"
+    Private Sub b_minimize_Copy_Click(sender As Object, e As RoutedEventArgs) Handles b_minimize_Copy.Click
+        If Me.WindowState = WindowState.Normal Then
+            Me.WindowState = WindowState.Maximized
+        Else
+            Me.WindowState = WindowState.Normal
+        End If
+    End Sub
 
-#End Region
+    Private Sub b_close_Click(sender As Object, e As RoutedEventArgs) Handles b_close.Click
+        Me.Close()
+    End Sub
 
 #Region "Controls"
 
 #End Region
 
-#Region "Information"
+#Region "Information Flow"
+
+#End Region
+
+#Region "SQL"
 
 #End Region
 
